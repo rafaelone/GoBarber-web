a37b0a1d0d3dcc10af0bed72a988ac670de53176
Rafael Sergio <rafa.sergio.02@live.com>
2020-04-25 16:51:07 -0300

    auth in storage

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <AuthContext.Provider value={{ name: 'Rafael' }}>
+    <AuthProvider>
       <SignIn />
-    </AuthContext.Provider>
+    </AuthProvider>
   </>
 );
 
